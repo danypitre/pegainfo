b2f0e66f165495e8611873190814e8bad3cc63cd
Christophe Chouinard <christophe.chouinard@gmail.com>
2022-05-25 17:45:03 -0400

    test router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}><App /></BrowserRouter>
+    <HashRouter basemname={`/${process.env.PUBLIC_URL}`}><App /></HashRouter>
       
   </React.StrictMode>
 );
