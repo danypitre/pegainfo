fea7cad790015f4053f3b0e371792d4092dc3b63
Christophe Chouinard <christophe.chouinard@gmail.com>
2022-05-25 13:45:09 -0400

    router correctifs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
 
 <div className='App'>
-  <BrowserRouter>
+  <BrowserRouter  basename={process.env.PUBLIC_URL}>
 
     <Routes> 
         <Route path='/' element={<HomePage/>} />
